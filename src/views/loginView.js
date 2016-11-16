e7c5cfb116e75c59a5f50d94cd42dacc61f8d1b4
rohghosh <ScoobyDoo_1986>
2016-11-16 16:04:57 +0530

    1. Login page empty field check implemented.

--- src/views/loginView.js
+++ src/views/loginView.js
@@ -10,29 +10,37 @@ export class LoginView extends View{
     getHtml() {
         var htmlContent =  (
             '<div id="loginView">' +
-            '<img id="logo" src="../resources/img/logo.png"/>' +
-            '<p class="caption"><%= message %></p>' +
-            '<div class="modal-dialog">'+
-            '<div class="loginmodal-container">' +
-            '<h1>Login to Your Account</h1><br>' +
-            '<input type="text" name="user" placeholder="Username">' +
-            '<input type="password" name="pass" placeholder="Password">' +
-            '<button id="loginBtn" type="submit" name="login" class="login loginmodal-submit">Login</button>' +
-            '<div class="login-help">' +
-            '<a href="#">Register</a> - <a href="#">Forgot Password</a>' +
-            '</div></div></div></div>'
+                '<img id="logo" src="../resources/img/logo.png"/>' +
+                '<p class="caption"><%= message %></p>' +
+                '<div class="modal-dialog">'+
+                    '<div class="loginmodal-container">' +
+                        '<h1>Login to Your Account</h1><br>' +
+                        '<div  id="errorMessage" class="auth-error error" role="alert" aria-live="assertive"><p style="margin:0px;">A username/password is required.</p></div>' +
+                        '<input id="userName" type="text" name="user" placeholder="Username">' +
+                        '<input id="password" type="password" name="pass" placeholder="Password">' +
+                        '<button id="loginBtn" type="submit" name="login" class="login loginmodal-submit">Login</button>' +
+                        '<div class="login-help">' +
+                            '<a href="#">Register</a> - <a href="#">Forgot Password</a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
         );
         return htmlContent;
     }
 
     bindDomEvents() {
         var $loginButton = this.$el.find('#loginBtn');
-        //var event = new CustomEvent('login:successful',{});
         var me = this;
         $loginButton.click(function(e){
             e.preventDefault();
-            console.log('Got click...');
-            //me.$el.trigger('login:successful',[{}]);
+            var userName = me.$el.find('#userName').val();
+            var password = me.$el.find('#password').val();
+            var errorMessage = me.$el.find('#errorMessage');
+            if(!userName || !password){
+                errorMessage.show();
+                return;
+            }
             me.trigger('login:successful',[{}]);
         });
     }
