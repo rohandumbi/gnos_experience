8155e5c1353011388b92683b30c4196a9c65fff8
rohghosh <ScoobyDoo_1986>
2016-12-29 13:12:56 +0530

    implemented sample arbor graph

--- src/views/workflowView.js
+++ src/views/workflowView.js
@@ -8,10 +8,27 @@ export class WorkflowView extends View{
     getHtml() {
         var htmlContent =  (
             '<div id="workflowView">' +
-                'Hello World' +
+                '<canvas id="viewport" width="1500" height="600"></canvas>' +
             '</div>'
         );
         return htmlContent;
+    }
+
+    render() {
+        super.render();
+        //var sys = arbor.ParticleSystem(2600, 512, 0.5);
+        var sys = arbor.ParticleSystem(1000, 400,1);
+        sys.parameters({gravity:true});
+        sys.renderer = Renderer(this.$el.find("#viewport"));
+        sys.screenPadding(20);
+        var animals = sys.addNode('Animals',{'color':'red','shape':'dot','label':'Animals'});
+
+        var dog = sys.addNode('dog',{'color':'green','shape':'dot','label':'dog'});
+        var cat = sys.addNode('cat',{'color':'blue','shape':'dot','label':'cat'});
+
+        sys.addEdge(animals, dog);
+        sys.addEdge(animals, cat);
+        return this;
     }
 
     bindDomEvents() {
