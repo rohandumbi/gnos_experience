212e9cd87e3a0cc28c75c760199b698ea8980852
rohghosh <ScoobyDoo_1986>
2017-03-12 04:22:22 +0530

    increased  workflow designer width

--- src/views/workflowView.js
+++ src/views/workflowView.js
@@ -338,6 +338,17 @@ export class WorkflowView extends View{
 
     onDomLoaded() {
         this.fetchUnits();
+        this.fitCanvasToContainer();
+    }
+
+    fitCanvasToContainer() {
+        var canvas = document.querySelector('canvas');
+        // Make it visually fill the positioned parent
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        // ...then set the internal size to match
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
     }
 
     initializeGraph(nodeData) {
