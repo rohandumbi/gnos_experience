c21d84ca3da03e0fec8149b472f003b1d2f4c966
rohghosh <ScoobyDoo_1986>
2017-04-21 17:17:44 +0530

    added dummy dual list for pit group details

--- src/views/pitGroupConceptView.js
+++ src/views/pitGroupConceptView.js
@@ -21,7 +21,6 @@ export class PitGroupConceptView extends View {
 
     onDomLoaded() {
         this.fetchPits();
-        this.bindDomEvents();
     }
 
     fetchPitGroups() {
@@ -34,9 +33,10 @@ export class PitGroupConceptView extends View {
                 data.forEach(function (pitGroup) {
                     $li = $('<a href="#" data-pitgroupname="' + pitGroup.name + '" draggable="false">' + pitGroup.name + '</a>');
                     $li.attr('title', pitGroup.name);
-                    $li.addClass('list-group-item list-group-item-success');
+                    $li.addClass('list-group-item list-group-item-success pit-group');
                     $liPitGroups.append($li);
                 });
+                that.bindDomEvents();
             },
             error: function (data) {
                 alert('Error fetching pit groups.');
@@ -54,7 +54,7 @@ export class PitGroupConceptView extends View {
                 data.forEach(function (pit) {
                     $li = $('<a href="#" data-pitname="' + pit.pitName + '" data-pitNo="' + pit.pitNo + '" draggable="false">' + pit.pitName + '</a>');
                     $li.attr('title', pit.pitName);
-                    $li.addClass('list-group-item list-group-item-info');
+                    $li.addClass('list-group-item list-group-item-info pit');
                     $liPits.append($li);
                 });
                 that.fetchPitGroups();
@@ -96,12 +96,64 @@ export class PitGroupConceptView extends View {
 
     bindDomEvents() {
         var that = this;
-        //this.bindDragEventsOnPits();
         this.$el.find('#add-pitgroup').click(function () {
             //alert("To display pit group definition form");
             var newGroupName = that.$el.find('#new_group_name').val();
             that.addPitGroup(newGroupName);
         });
+        this.$el.find('.pit-group').click(function (event) {
+            that.loadPitGroupDetails({name: $(this).data('pitgroupname')});
+            that.$el.find('.pit-group').removeClass('active');
+            $(this).addClass('active');
+        });
+        this.bindPitGroupEvents();
+    }
+
+    bindPitGroupEvents() {
+        this.$el.on('click', '.child-pits .list-group-item', function () {
+            $(this).toggleClass('active');
+        });
+        this.$el.on('click', '.all-pits .list-group-item', function () {
+            $(this).toggleClass('active');
+        });
+        this.$el.find('.list-arrows button').click(function () {
+            var $button = $(this), actives = '';
+            if ($button.hasClass('move-left')) {
+                actives = $('.list-right ul li.active');
+                actives.clone().appendTo('.list-left ul');
+                actives.remove();
+            } else if ($button.hasClass('move-right')) {
+                actives = $('.list-left ul li.active');
+                actives.clone().appendTo('.list-right ul');
+                actives.remove();
+            }
+        });
+        this.$el.find('.dual-list .selector').click(function () {
+            var $checkBox = $(this);
+            if (!$checkBox.hasClass('selected')) {
+                $checkBox.addClass('selected').closest('.well').find('ul li:not(.active)').addClass('active');
+                $checkBox.children('i').removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+            } else {
+                $checkBox.removeClass('selected').closest('.well').find('ul li.active').removeClass('active');
+                $checkBox.children('i').removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+            }
+        });
+        this.$el.find('[name="SearchDualList"]').keyup(function (e) {
+            var code = e.keyCode || e.which;
+            if (code == '9') return;
+            if (code == '27') $(this).val(null);
+            var $rows = $(this).closest('.dual-list').find('.list-group li');
+            var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+            $rows.show().filter(function () {
+                var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                return !~text.indexOf(val);
+            }).hide();
+        });
+    }
+
+    loadPitGroupDetails(options) {
+        this.loadedPitGroup = options.name;
+        //alert('To Load: ' + this.loadedPitGroup);
     }
 
     addPitGroup(groupName) {
@@ -119,7 +171,7 @@ export class PitGroupConceptView extends View {
                 var $liPitGroups = that.$el.find('#pit-group-list');
                 var $li = $('<a href="#" data-pitgroupname="' + data.name + '" draggable="false">' + data.name + '</a>');
                 $li.attr('title', data.name);
-                $li.addClass('list-group-item list-group-item-success');
+                $li.addClass('list-group-item list-group-item-success pit-group');
                 $liPitGroups.prepend($li);
             },
             error: function (data) {
